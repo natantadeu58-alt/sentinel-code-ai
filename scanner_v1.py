59626a0144269bfc7e47b2d33aebca2859969e27
natantadeu58-alt <natantadeu58@gmail.com>
2026-06-24 03:58:35 -0300

    Refactor analisar_link function and main execution

--- scanner_v1.py
+++ scanner_v1.py
@@ -5,17 +5,13 @@ def analisar_link(url):
     if re.search(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', url):
         pontos_suspeitos += 1
         motivos.append("O link contém um endereço IP em vez de um domínio.")
-        
     return pontos_suspeitos, motivos
-
 if __name__ == "__main__":
     print("\n" + "=" * 30)
     print("Detector de Phishing v1.0")
     print("=" * 30 + "\n")
-    
     url = input("Digite ou cole a URL para análise: ")
     print(f"Analisando o link: {url}")
-    
     pontos, motivos = analisar_link(url)
     if pontos >= 1:
         print("\nlink perigo: forte indício de phishing")
